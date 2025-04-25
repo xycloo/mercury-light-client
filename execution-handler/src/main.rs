e4befc6f3962a4cbe46d13a8e980f5e57eb52c20
itsfederico <federicodeponti@zohomail.eu>
2025-04-25 18:08:16 -0300

    finished the uploading program logic

--- execution-handler/src/main.rs
+++ execution-handler/src/main.rs
@@ -19,6 +19,112 @@ struct ExecuteRequest {
     // ... any extra params ...
 }
 
+#[derive(Deserialize, Serialize, Debug)]
+struct CodeUploadClient {
+    code: Option<Vec<u8>>,
+    contract: Option<bool>,
+    contracts: Option<Vec<String>>,
+}
+
+fn with_db(pool: Pool) -> impl Filter<Extract = (Pool,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || pool.clone())
+}
+
+pub async fn insert_zephyr_code<'a>(
+    code: &Vec<u8>,
+    is_contract: bool,
+    contracts: Vec<String>,
+) -> Result<(), Error> {
+    let postgres_args: String = env::var("INGESTOR_DB").unwrap();
+
+    let (client, connection) = tokio_postgres::connect(&postgres_args, NoTls)
+        .await
+        .unwrap();
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    /*
+    let delete_stmt = client
+        .prepare_typed(
+            "DELETE FROM public.zephyr_programs WHERE user_id = $1 AND project_name = $2",
+            &[
+                tokio_postgres::types::Type::INT4,
+                tokio_postgres::types::Type::TEXT,
+            ],
+        )
+        .await
+        .unwrap();
+
+    client
+        .execute(&delete_stmt, &[&user, &project_name])
+        .await
+        .unwrap();
+    */
+
+    let stmt = client
+        .prepare_typed(
+            "INSERT INTO public.zephyr_programs (code, is_contract, contracts) VALUES ($1, $2, $3)",
+            &[
+                tokio_postgres::types::Type::BYTEA,
+                tokio_postgres::types::Type::BOOL,
+                tokio_postgres::types::Type::TEXT_ARRAY,
+            ],
+        )
+        .await
+        .unwrap();
+
+    client
+        .execute(
+            &stmt,
+            &[
+                &code.as_slice(),
+                &is_contract,
+                &contracts,
+            ],
+        )
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn upload(
+    code: &Vec<u8>,
+    is_contract: bool,
+    contracts: Vec<String>,
+) -> Result<(), crate::Error> {
+
+    let mut config = wasmi::Config::default();
+    let stack_limits = StackLimits::new(
+        MIN_VALUE_STACK_HEIGHT,
+        MAX_VALUE_STACK_HEIGHT,
+        MAX_RECURSION_DEPTH,
+    )
+    .map_err(|_| Error::InvalidWASMCode)?;
+
+    // TODO: decide which post-mvp features to override.
+    // For now we use wasmtime's defaults.
+    config.consume_fuel(true);
+    config.set_stack_limits(stack_limits);
+    config.compilation_mode(wasmi::CompilationMode::Lazy);
+
+    let engine = Engine::new(&config);
+    Module::validate(&engine, &code).map_err(|_| Error::InvalidWASMCode)?;
+
+    database::insert_zephyr_code(
+        code,
+        is_contract,
+        contracts,
+    )
+    .await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<UserLogPair>();
@@ -29,7 +135,7 @@ async fn main() {
 
     // check the upload function: start from there
     // understand rhe best way to start the warp server 
-    let upload = warp::post()
+    let upload_zephyr_code = warp::post()
         .and(warp::path("zephyr_upload"))
         .and(with_tx(arc_tx.clone()))
         .and(with_db(pg_pool.clone()))
@@ -37,9 +143,12 @@ async fn main() {
         .and_then(
             |logger: Arc<UnboundedSender<UserLogPair>>,
              mut sub: CodeUploadClient| async move {
-                if let Ok(id) = authed {
-                    let program = ZephyrClient::new(id as i32);
-                    let upload = program.upload(&sub.code.unwrap(), sub.force_replace).await;
+                let upload = upload(
+                    &sub.code.unwrap(),
+                    sub.contract.unwrap_or(false),
+                    sub.contracts.unwrap_or(vec![]),
+                )
+                .await;
 
                     if let Err(_) = upload {
                         return Ok::<WithStatus<String>, Rejection>(warp::reply::with_status(
@@ -49,7 +158,6 @@ async fn main() {
                     }
 
                     logger.send(UserLogPair {
-                        user: id as i64,
                         log: MercuryLog {
                             level: LogLevel::Debug,
                             message: "Uploaded new Zephyr binary".into(),
@@ -61,13 +169,6 @@ async fn main() {
                         "true".to_string(),
                         StatusCode::CREATED,
                     ))
-                } else {
-                    println!("non authed");
-                    Ok::<WithStatus<String>, Rejection>(warp::reply::with_status(
-                        "Invalid JWT Auth".to_string(),
-                        StatusCode::UNAUTHORIZED,
-                    ))
-                }
             },
         );
 
@@ -78,14 +179,12 @@ async fn main() {
         .and(warp::path("execute"))
         */
         
-    // 4) Combine and run
+        let routes = upload.with(warp::log("zephyr_uploader"));
+
+        /*
     let routes = upload.or(execute)
         .with(warp::log("zephyr_uploader"));
 
+        */
     warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
-}
-
-// Helper to inject the DB pool
-fn with_db(pool: Pool) -> impl Filter<Extract = (Pool,), Error = std::convert::Infallible> + Clone {
-    warp::any().map(move || pool.clone())
 }
